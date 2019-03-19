34b7498ce1ed48413a771eb7ceedfd2d2c0bc7f2
wuyiwen <563921065@qq.com>
2019-03-19 15:52:49 +0800

    修复babel报错

--- src/view/App/index.js
+++ src/view/App/index.js
@@ -5,7 +5,7 @@ import Router from '../Router';
 //引入antd样式
 import 'antd/dist/antd.css';
 import zhCN from 'antd/lib/locale-provider/zh_CN';
-// import styles from  './index.less';
+import styles from  './index.less';
 
 const { Header, Content, Footer } = Layout;
 
@@ -17,14 +17,14 @@ class App extends React.Component {
         <BrowserRouter>
           <Layout>
             <Header>
-              <nav className='nav-bar'>
-                <NavLink to='/' exact activeClassName='nav-—selected' >首页</NavLink>
-                <NavLink to='/demo1' activeClassName='nav-—selected'>练习1</NavLink>
-                <NavLink to='/demo2' activeClassName='nav-—selected'>练习2</NavLink>
+              <nav className={styles['nav-bar']}>
+                <NavLink to='/' exact activeClassName={styles['nav-—selected']} >首页</NavLink>
+                <NavLink to='/demo1' activeClassName={styles['nav-—selected']}>练习1</NavLink>
+                <NavLink to='/demo2' activeClassName={styles['nav-—selected']}>练习2</NavLink>
               </nav>
             </Header>
             <Content>
-              <div className='content-container'>
+              <div className={styles['content-container']}>
                 <Router />
               </div>
             </Content>
